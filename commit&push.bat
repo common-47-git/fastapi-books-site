9d299beed205dc1cc90d80f99f26cb01d112e589
Andrew <common4704@gmail.com>
2024-09-08 20:59:38 +0300

    commit

--- commit&push.bat
+++ commit&push.bat
@@ -1,15 +1,13 @@
 @echo off
+
 set /p if_commit="Do you want to commit the changes? (yes/no): "
 
 if /i "%if_commit%"=="yes" (
-    set /p commit_msg="Enter the commit message: "
-    if "%commit_msg%"=="" (
-        set commit_msg="commit"
-    )
     git add .
-    git commit -m "%commit_msg%"
-    git push -u origin master
+    git commit -m "commit"
+    git push -u origin main
 ) else (
     echo No commit made.
 )
+
 set /p commit_msg="Exit..."
